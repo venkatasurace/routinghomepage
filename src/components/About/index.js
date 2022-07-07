3c71d43a473d4999782350a9e0602c43bc4ed430
venkatasurace <you@example.com>
2022-07-07 16:44:40 +0000

    routing

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="home"
+      className="about-img"
+    />
+    <h1 className="about-heading">Home</h1>
+  </div>
+)
+
+export default About
